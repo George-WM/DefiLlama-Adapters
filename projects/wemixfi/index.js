76fef2c38a66fc9632ae6afff73069b58bd1e912
George-WM <122424578+George-WM@users.noreply.github.com>
2023-02-21 09:55:27 +0900

    Update index.js

--- projects/wemixfi/index.js
+++ projects/wemixfi/index.js
@@ -1,1 +1,10 @@
+const {getUniTVL} = require("../helper/unknownTokens");
+const { stakings } = require("../helper/staking");
 
+const factory = "0xe1F36C7B919c9f893E2Cd30b471434Aa2494664A";
+
+module.exports = {
+  wemix: {
+    tvl: getUniTVL({ chain: 'wemix', factory, })
+  }
+};
